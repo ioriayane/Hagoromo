a00435feb53adbfc9856d6df536170438f8e854a
Takayuki Orito <iori.ayane@gmail.com>
2023-06-11 17:43:21 +0900

    ビルドスクリプト追加（Windows用）

---
+++ scripts/build.bat
@@ -0,0 +1,65 @@
+@echo off
+
+set CWD=%CD%
+set QT_BIN_FOLDER=%1
+
+if '%QT_BIN_FOLDER%'=='' goto HELP
+goto MAIN
+
+REM --- help -----------------------------
+:HELP
+echo build.bat QT_BIN_FOLDER
+echo ex:
+echo   build.bat path\to\Qt\5.15.2\msvc2019_64\bin
+
+goto QUIT
+
+REM --- main -----------------------------
+:MAIN
+
+set JOM_BIN_FOLDER=%QT_BIN_FOLDER%\..\..\..\Tools\QtCreator\bin\jom
+set VS_SETUP_BAT="C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+set BUILD_FOLDER=build-hagoromo
+set DEPLOY_FOLDER=deploy-hagoromo
+
+
+REM --- check path -------
+qmake -v
+if ERRORLEVEL 1 set path=%QT_BIN_FOLDER%;%PATH%
+jom /version
+if ERRORLEVEL 1 set path=%JOM_BIN_FOLDER%;%PATH%
+nmake /? /c
+if ERRORLEVEL 1 call %VS_SETUP_BAT%
+
+REM --- make folder -------
+if EXIST %BUILD_FOLDER% rmdir /s /q %BUILD_FOLDER%
+if NOT ERRORLEVEL 0 goto QUIT
+mkdir %BUILD_FOLDER%
+if NOT ERRORLEVEL 0 goto QUIT
+
+if EXIST %DEPLOY_FOLDER% rmdir /s /q %DEPLOY_FOLDER%
+if NOT ERRORLEVEL 0 goto QUIT
+mkdir %DEPLOY_FOLDER%
+if NOT ERRORLEVEL 0 goto QUIT
+mkdir %DEPLOY_FOLDER%\hagoromo
+if NOT ERRORLEVEL 0 goto QUIT
+
+REM --- build -------
+cd %BUILD_FOLDER%
+
+qmake ..\app\app.pro
+if NOT ERRORLEVEL 0 goto QUIT
+
+jom
+if NOT ERRORLEVEL 0 goto QUIT
+
+cd %CWD%
+
+
+REM --- deploy -------
+copy %BUILD_FOLDER%\release\Hagoromo.exe %DEPLOY_FOLDER%\hagoromo\
+copy %BUILD_FOLDER%\release\*.dll %DEPLOY_FOLDER%\hagoromo\
+windeployqt --qmldir app\qml %DEPLOY_FOLDER%\hagoromo\Hagoromo.exe
+
+
+:QUIT
