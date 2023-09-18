6eb3b5836b3d0b892ce153825b6afa9da2b1c280
Takayuki Orito <iori.ayane@gmail.com>
2023-09-18 18:02:12 +0900

    バージョン情報更新 (#55)

--- app/main.cpp
+++ app/main.cpp
@@ -46,7 +46,7 @@ int main(int argc, char *argv[])
     app.setOrganizationName(QStringLiteral("relog"));
     app.setOrganizationDomain(QStringLiteral("hagoromo.relog.tech"));
     app.setApplicationName(QStringLiteral("Hagoromo"));
-    app.setApplicationVersion(QStringLiteral("0.11.0"));
+    app.setApplicationVersion(QStringLiteral("0.12.0"));
 #ifndef HAGOROMO_RELEASE_BUILD
     app.setApplicationVersion(app.applicationVersion() + "d");
 #endif
