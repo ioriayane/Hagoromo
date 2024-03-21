af28b18ebe439ed5db660630a73226a5b070900b
Takayuki Orito <iori.ayane@gmail.com>
2024-03-21 22:44:36 +0900

    バージョン情報更新

--- app/main.cpp
+++ app/main.cpp
@@ -72,7 +72,7 @@ int main(int argc, char *argv[])
     app.setOrganizationName(QStringLiteral("relog"));
     app.setOrganizationDomain(QStringLiteral("hagoromo.relog.tech"));
     app.setApplicationName(QStringLiteral("Hagoromo"));
-    app.setApplicationVersion(QStringLiteral("0.25.0"));
+    app.setApplicationVersion(QStringLiteral("0.26.0"));
 #ifndef HAGOROMO_RELEASE_BUILD
     app.setApplicationVersion(app.applicationVersion() + "d");
 #endif
--- web/content/docs/release-note.en.md
+++ web/content/docs/release-note.en.md
@@ -8,6 +8,8 @@ description: This is a multi-column Bluesky client.
 
 ## 2024
 
+### v0.26.0 - 2024/3/21
+
 - Add
   - Support muted word
   - Add list of blocked and muted accounts
