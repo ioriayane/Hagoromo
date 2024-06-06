105637018d64e7d24e9fd2015a7519dbd25d05c1
Takayuki Orito <iori.ayane@gmail.com>
2024-06-07 03:15:05 +0900

    フォロー・フォロワーの途中の変更に追従

--- lib/realtime/followerspostselector.cpp
+++ lib/realtime/followerspostselector.cpp
@@ -8,7 +8,36 @@ bool FollowersPostSelector::judge(const QJsonObject &object)
 {
     // 他人の自分をフォローするデータのとき追加する
     // deleteのときは消す
-
+    if (!isMy(object)) {
+        QJsonObject op = getOperation(object, "app.bsky.graph.follow");
+        if (!op.isEmpty()) {
+            QString action = op.value("action").toString();
+            if (action == "create") {
+                QString path = op.value("path").toString();
+                QJsonObject block = getBlock(object, path);
+                if (!block.isEmpty()) {
+                    QString subject = block.value("value").toObject().value("subject").toString();
+                    if (subject == did()) {
+                        UserInfo user;
+                        user.did = getRepo(object);
+                        user.rkey = extractRkey(path);
+                        m_followers[user.did] = user;
+                    }
+                }
+            } else if (action == "delete") {
+                QString rkey = extractRkey(op.value("path").toString());
+                if (!rkey.isEmpty()) {
+                    QString user_did = getRepo(object);
+                    for (const auto &user : qAsConst(m_followers)) {
+                        if (user.rkey == rkey && user.did == user_did) {
+                            m_followers.remove(user.did);
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
     return isTarget(object) && m_followers.contains(getRepo(object));
 }
 
