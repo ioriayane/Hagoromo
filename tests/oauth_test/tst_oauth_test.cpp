956709896aba3dbd79ebf51ed474d8a87900fba5
Takayuki Orito <iori.ayane@gmail.com>
2024-08-26 23:32:28 +0900

    単体テストでjwtの検証をできるように変更

--- tests/oauth_test/tst_oauth_test.cpp
+++ tests/oauth_test/tst_oauth_test.cpp
@@ -121,6 +121,7 @@ void oauth_test::verify_jwt(const QByteArray &jwt)
     const QByteArrayList jwt_parts = jwt.split('.');
     QVERIFY2(jwt_parts.length() == 3, jwt);
 
+    QByteArray message = jwt_parts[0] + '.' + jwt_parts[1];
     QByteArray sig = QByteArray::fromBase64(
             jwt_parts.last(), QByteArray::Base64UrlEncoding | QByteArray::OmitTrailingEquals);
     QVERIFY2(sig.length() == 64, QString::number(sig.length()).toLocal8Bit());
@@ -137,12 +138,26 @@ void oauth_test::verify_jwt(const QByteArray &jwt)
     QVERIFY(ec_sig_s != NULL);
 
     ECDSA_SIG *ec_sig = ECDSA_SIG_new();
-
     QVERIFY(ECDSA_SIG_set0(ec_sig, ec_sig_r, ec_sig_s) == 1);
 
-    unsigned char *der_sig;
-    int sig_len = i2d_ECDSA_SIG(ec_sig, &der_sig);
-    QVERIFY(sig_len > 0);
+    unsigned char *der_sig = NULL;
+    int der_sig_len = i2d_ECDSA_SIG(ec_sig, &der_sig);
+    QVERIFY(der_sig_len > 0);
+
+    FILE *fp = fopen("c:\\temp\\public_key.pem", "r");
+    QVERIFY(fp != NULL);
+    EVP_PKEY *pkey = PEM_read_PUBKEY(fp, nullptr, nullptr, nullptr);
+    fclose(fp);
+    QVERIFY(pkey != NULL);
+
+    // ECDSA署名の検証
+    EVP_MD_CTX *mdctx = EVP_MD_CTX_new();
+    QVERIFY(EVP_DigestVerifyInit(mdctx, nullptr, EVP_sha256(), nullptr, pkey) > 0);
+    QVERIFY(EVP_DigestVerifyUpdate(mdctx, message.constData(), message.length()) > 0);
+    QVERIFY(EVP_DigestVerifyFinal(mdctx, der_sig, der_sig_len) > 0);
+
+    EVP_MD_CTX_free(mdctx);
+    EVP_PKEY_free(pkey);
 
     BN_free(ec_sig_r);
     BN_free(ec_sig_s);
