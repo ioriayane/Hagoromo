dcecdd172c2432528cfdee40c90748b0817ae047
Takayuki Orito <iori.ayane@gmail.com>
2025-05-02 03:39:52 +0900

    Windows版のビルドをNinjaに変更

--- scripts/build.bat
+++ scripts/build.bat
@@ -2,14 +2,13 @@
 
 set CWD=%CD%
 set QTDIR=%1
-set QT_BIN_FOLDER=%QTDIR%\bin
 
-if '%QT_BIN_FOLDER%'=='' goto HELP
+if '%QTDIR%'=='' goto HELP
 goto MAIN
 
 REM --- help -----------------------------
 :HELP
-echo build.bat QT_BIN_FOLDER
+echo build.bat QTDIR
 echo ex:
 echo   build.bat path\to\Qt\6.8.1\msvc2019_64
 
@@ -23,7 +22,6 @@ IF not defined  VS_REDIST_FOLDER set VS_REDIST_FOLDER="C:\Program Files\Microsof
 set SRC_FOLDER=..
 set BUILD_FOLDER=build-hagoromo
 set DEPLOY_FOLDER=deploy-hagoromo
-set CMAKE_PLATFORM=x64
 set OPENSSL_FOLDER=%QTDIR%/../../Tools/OpenSSLv3/Win_x64
 
 echo VS_SETUP_BAT=%VS_SETUP_BAT%
@@ -33,11 +31,8 @@ REM --- build deps -------
 cmd.exe /c %CWD%/scripts/build_zlib.bat
 
 REM --- check path -------
-qmake -v
-if ERRORLEVEL 1 set path=%QT_BIN_FOLDER%;%PATH%
 nmake /? /c
 if ERRORLEVEL 1 call %VS_SETUP_BAT%
-
 
 REM --- make folder -------
 if EXIST %BUILD_FOLDER% rmdir /s /q %BUILD_FOLDER%
@@ -49,33 +44,45 @@ if EXIST %DEPLOY_FOLDER% rmdir /s /q %DEPLOY_FOLDER%
 if NOT ERRORLEVEL 0 goto QUIT
 mkdir %DEPLOY_FOLDER%
 if NOT ERRORLEVEL 0 goto QUIT
-mkdir %DEPLOY_FOLDER%\hagoromo
-if NOT ERRORLEVEL 0 goto QUIT
-
 
 
 REM --- build -------
 cd %BUILD_FOLDER%
 
-cmake %SRC_FOLDER% -G "Visual Studio 17 2022" -DCMAKE_INSTALL_PREFIX="%DEPLOY_FOLDER%" -DHAGOROMO_RELEASE_BUILD=ON -DCMAKE_BUILD_TYPE:STRING=Release -A %CMAKE_PLATFORM%
+cmake .. -G Ninja -DCMAKE_PREFIX_PATH:PATH='%QTDIR%' ^
+    -DCMAKE_INSTALL_PREFIX:PATH='..\%DEPLOY_FOLDER%\hagoromo' ^
+    -DCMAKE_INSTALL_LIBDIR:PATH='.' ^
+    -DCMAKE_INSTALL_BINDIR:PATH='.' ^
+    -DHAGOROMO_RELEASE_BUILD=ON ^
+    -DCMAKE_BUILD_TYPE:STRING=Release
 if NOT ERRORLEVEL 0 goto QUIT
 
 cmake --build . --target update_translations
 if NOT ERRORLEVEL 0 goto QUIT
 
-cmake --build . --config RELEASE --target INSTALL
+cmake --build . --target install
 if NOT ERRORLEVEL 0 goto QUIT
-
 
 cd %CWD%
 
-
 REM --- deploy -------
-copy %BUILD_FOLDER%\app\release\Hagoromo.exe %DEPLOY_FOLDER%\hagoromo\
-copy %BUILD_FOLDER%\app\release\*.dll %DEPLOY_FOLDER%\hagoromo\
-copy zlib\bin\zlib.dll %DEPLOY_FOLDER%\hagoromo\
-windeployqt --qmldir app\qml --openssl-root %OPENSSL_FOLDER% --no-translations %DEPLOY_FOLDER%\hagoromo\Hagoromo.exe
-python3 scripts\copymsvcfiles.py %VS_REDIST_FOLDER% %DEPLOY_FOLDER%\hagoromo\
+%QTDIR%\bin\windeployqt --qmldir app\qml ^
+                        --openssl-root %OPENSSL_FOLDER% ^
+                        --no-translations ^
+                        --no-virtualkeyboard ^
+                        --no-quick3dutils ^
+                        --no-quickcontrols2fluentwinui3styleimpl ^
+                        --no-quickcontrols2fusion ^
+                        --no-quickcontrols2fusionstyleimpl ^
+                        --no-quickcontrols2imagine ^
+                        --no-quickcontrols2imaginestyleimpl ^
+                        --no-quickcontrols2universal ^
+                        --no-quickcontrols2universalstyleimpl ^
+                        --no-quickcontrols2windowsstyleimpl ^
+                        --exclude-plugins qopensslbackend,scene2d ^
+                        --release ^
+                        %DEPLOY_FOLDER%\hagoromo\Hagoromo.exe
+python3 scripts\copymsvcfiles.py %VS_REDIST_FOLDER% %DEPLOY_FOLDER%\hagoromo
 
 for /f "usebackq delims=" %%A in (`PowerShell -Command "((Get-Content app/main.cpp) -match 'app.setApplicationVersion' | Select-String -Pattern '[0-9]+\.[0-9]+\.[0-9]+' -AllMatches).Matches.Value"`) do set VERSION_NO=%%A
 PowerShell -Command "Compress-Archive -Path deploy-hagoromo\hagoromo -DestinationPath deploy-hagoromo\hagoromo_%VERSION_NO%_windows.zip"
