db0808505085dfabe42d8ddb0a1098a781590479
Takayuki Orito <iori.ayane@gmail.com>
2025-05-04 16:16:47 +0900

    Linuxのテスト実行にオプションを追加（修正）

--- tests/tests.cmake
+++ tests/tests.cmake
@@ -3,8 +3,8 @@ target_compile_definitions(${PROJECT_NAME} PRIVATE
     HAGOROMO_UNIT_TEST
 )
 
-if(LINUX)
-    set_tests_properties(${PROJECT_NAME} PROPERTIES
-        "--platform" "offscreen"
-    )
+if(UNIX)
+    add_test(NAME ${PROJECT_NAME} COMMAND ${PROJECT_NAME} --platform offscreen)
+else()
+    add_test(NAME ${PROJECT_NAME} COMMAND ${PROJECT_NAME})
 endif()
